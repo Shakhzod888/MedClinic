b11891fa1a5737799a0b07ddb92f5b7f393c964b
Shakhzod <ShakhzodTurabidinov@gmail.com>
2023-08-14 21:32:35 +0500

    finish code

---
+++ src/components/pages/doctorsTeam/doctorsTeam.jsx
@@ -0,0 +1,256 @@
+import { Box, Flex, Paper, Text } from "@mantine/core";
+import Doc_1 from "../assets/doc1.png";
+import Doc_2 from "../assets/doc2.png";
+import Doc_3 from "../assets/doc3.png";
+import Doc_4 from "../assets/doc4.png";
+import Doc_5 from "../assets/doc5.png";
+import Doc_6 from "../assets/doc6.png";
+import Doc_7 from "../assets/doc7.png";
+import Doc_8 from "../assets/doc8.png";
+import Doc_9 from "../assets/doc9.png";
+import Doc_10 from "../assets/doc10.png";
+
+import Consultations from "../assets/consultations.png";
+import React from "react";
+
+function DoctorsTeam() {
+  return (
+    <>
+      <Box maw="100%" w="100%" h="160vh">
+        <Text ta="center" fw={500} lts={1} pt={20} fz={30} c="#1580EB">
+          Team Doctor
+        </Text>
+        <Text ta="center" fz={40} fw={600}>
+          Meet Our <span className="stative">Specialists</span>
+        </Text>
+        <Box mt={40}>
+          <Flex align="center" justify="space-around">
+            <Paper
+              shadow="0px 2px 10px 0px rgba(0, 0, 0, 0.25)"
+              bg="#ffffff"
+              w={220}
+              h={250}
+            >
+              <Flex justify="center" align="center" direction="column">
+                <Flex className="docBox" bg="#A9CDF0" justify="center" w="80%">
+                  <img src={Doc_1} alt="" />
+                </Flex>
+                <Text fz={20} pt={10} align="center" ta="center">
+                  Dylan James
+                </Text>
+                <Text fz={20} c="#A4A4A4" align="center" ta="center">
+                  General Sugery
+                </Text>
+              </Flex>
+            </Paper>
+            <Paper
+              shadow="0px 2px 10px 0px rgba(0, 0, 0, 0.25)"
+              bg="#ffffff"
+              w={220}
+              h={250}
+            >
+              <Flex justify="center" align="center" direction="column">
+                <Flex className="docBox" bg="#A9CDF0" justify="center" w="80%">
+                  <img src={Doc_2} alt="" />
+                </Flex>
+                <Text fz={20} pt={10} align="center" ta="center">
+                  Dylan James
+                </Text>
+                <Text fz={20} c="#A4A4A4" align="center" ta="center">
+                  General Sugery
+                </Text>
+              </Flex>
+            </Paper>
+            <Paper
+              shadow="0px 2px 10px 0px rgba(0, 0, 0, 0.25)"
+              bg="#ffffff"
+              w={220}
+              h={250}
+            >
+              <Flex justify="center" align="center" direction="column">
+                <Flex className="docBox" bg="#A9CDF0" justify="center" w="80%">
+                  <img src={Doc_3} alt="" />
+                </Flex>
+                <Text fz={20} pt={10} align="center" ta="center">
+                  Dylan James
+                </Text>
+                <Text fz={20} c="#A4A4A4" align="center" ta="center">
+                  General Sugery
+                </Text>
+              </Flex>
+            </Paper>
+            <Paper
+              shadow="0px 2px 10px 0px rgba(0, 0, 0, 0.25)"
+              bg="#ffffff"
+              w={220}
+              h={250}
+            >
+              <Flex justify="center" align="center" direction="column">
+                <Flex className="docBox" bg="#A9CDF0" justify="center" w="80%">
+                  <img src={Doc_4} alt="" />
+                </Flex>
+                <Text fz={20} pt={10} align="center" ta="center">
+                  Dylan James
+                </Text>
+                <Text fz={20} c="#A4A4A4" align="center" ta="center">
+                  General Sugery
+                </Text>
+              </Flex>
+            </Paper>
+            <Paper
+              shadow="0px 2px 10px 0px rgba(0, 0, 0, 0.25)"
+              bg="#ffffff"
+              w={220}
+              h={250}
+            >
+              <Flex justify="center" align="center" direction="column">
+                <Flex className="docBox" bg="#A9CDF0" justify="center" w="80%">
+                  <img src={Doc_5} alt="" />
+                </Flex>
+                <Text fz={20} pt={10} align="center" ta="center">
+                  Dylan James
+                </Text>
+                <Text fz={20} c="#A4A4A4" align="center" ta="center">
+                  General Sugery
+                </Text>
+              </Flex>
+            </Paper>
+          </Flex>
+        </Box>
+        <Box mt={40}>
+          <Flex align="center" justify="space-around">
+            <Paper
+              shadow="0px 2px 10px 0px rgba(0, 0, 0, 0.25)"
+              bg="#ffffff"
+              w={220}
+              h={250}
+            >
+              <Flex justify="center" align="center" direction="column">
+                <Flex className="docBox" bg="#A9CDF0" justify="center" w="80%">
+                  <img src={Doc_6} alt="" />
+                </Flex>
+                <Text fz={20} pt={10} align="center" ta="center">
+                  Dylan James
+                </Text>
+                <Text fz={20} c="#A4A4A4" align="center" ta="center">
+                  General Sugery
+                </Text>
+              </Flex>
+            </Paper>
+            <Paper
+              shadow="0px 2px 10px 0px rgba(0, 0, 0, 0.25)"
+              bg="#ffffff"
+              w={220}
+              h={250}
+            >
+              <Flex justify="center" align="center" direction="column">
+                <Flex className="docBox" bg="#A9CDF0" justify="center" w="80%">
+                  <img src={Doc_7} alt="" />
+                </Flex>
+                <Text fz={20} pt={10} align="center" ta="center">
+                  Dylan James
+                </Text>
+                <Text fz={20} c="#A4A4A4" align="center" ta="center">
+                  General Sugery
+                </Text>
+              </Flex>
+            </Paper>
+            <Paper
+              shadow="0px 2px 10px 0px rgba(0, 0, 0, 0.25)"
+              bg="#ffffff"
+              w={220}
+              h={250}
+            >
+              <Flex justify="center" align="center" direction="column">
+                <Flex className="docBox" bg="#A9CDF0" justify="center" w="80%">
+                  <img src={Doc_8} alt="" />
+                </Flex>
+                <Text fz={20} pt={10} align="center" ta="center">
+                  Dylan James
+                </Text>
+                <Text fz={20} c="#A4A4A4" align="center" ta="center">
+                  General Sugery
+                </Text>
+              </Flex>
+            </Paper>
+            <Paper
+              shadow="0px 2px 10px 0px rgba(0, 0, 0, 0.25)"
+              bg="#ffffff"
+              w={220}
+              h={250}
+            >
+              <Flex justify="center" align="center" direction="column">
+                <Flex className="docBox" bg="#A9CDF0" justify="center" w="80%">
+                  <img src={Doc_9} alt="" />
+                </Flex>
+                <Text fz={20} pt={10} align="center" ta="center">
+                  Dylan James
+                </Text>
+                <Text fz={20} c="#A4A4A4" align="center" ta="center">
+                  General Sugery
+                </Text>
+              </Flex>
+            </Paper>
+            <Paper
+              shadow="0px 2px 10px 0px rgba(0, 0, 0, 0.25)"
+              bg="#ffffff"
+              w={220}
+              h={250}
+            >
+              <Flex justify="center" align="center" direction="column">
+                <Flex className="docBox" bg="#A9CDF0" justify="center" w="80%">
+                  <img src={Doc_10} alt="" />
+                </Flex>
+                <Text fz={20} pt={10} align="center" ta="center">
+                  Dylan James
+                </Text>
+                <Text fz={20} c="#A4A4A4" align="center" ta="center">
+                  General Sugery
+                </Text>
+              </Flex>
+            </Paper>
+          </Flex>
+        </Box>
+
+        <Box>
+          <Flex mt={80} justify="center" align="center">
+            <Paper radius="50px" w="90%" h="49vh" bg="#E5F1FA">
+              <Flex justify="center" gap={30} align="center">
+                <Box>
+                  <img className="consultations" src={Consultations} alt="" />
+                </Box>
+                <Box mt={60}>
+                  <Paper radius={30} w="200px" h="50px" bg="#ffffff">
+                    <Text ta="center" align="center" fz={30} fw={600}>
+                      {" "}
+                      Med<span className="stative">Clinic</span>
+                    </Text>
+                  </Paper>
+                  <Text c="#1580eb" pt={10} fw={600} fz={40} w={500}>
+                    Free Online Consultations Starting at $49/month
+                  </Text>
+                  <Box pt={20}>
+                    <Paper radius={30} w={250} h="50px" bg="#036">
+                      <Text
+                        pt={5}
+                        ta="center"
+                        c="#ffffff"
+                        align="center"
+                        fz={26}
+                        fw={600}
+                      >
+                        Get membership
+                      </Text>
+                    </Paper>
+                  </Box>
+                </Box>
+              </Flex>
+            </Paper>
+          </Flex>
+        </Box>
+      </Box>
+    </>
+  );
+}
+
+export default DoctorsTeam;
--- src/components/pages/pages.jsx
+++ src/components/pages/pages.jsx
@@ -2,12 +2,18 @@ import React from "react";
 import Home from "./home/home";
 import { Box } from "@mantine/core";
 import Services from "./services/services";
+import Appointments from "./appointments/appointments";
+import DoctorsTeam from "./doctorsTeam/doctorsTeam";
+import Thoughts from "./thoughts/thoughts";
 
 function Pages() {
   return (
     <Box>
       <Home />
       <Services />
+      <Appointments />
+      <DoctorsTeam />
+      <Thoughts />
     </Box>
   );
 }
